fcd4e72adfbdd05981b4fe939c7154952b3f8f91
Jacob <jacobcons@gmail.com>
2024-07-11 16:53:23 +0000

    more create booking validation

--- handlers/bookings.handlers.go
+++ handlers/bookings.handlers.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"github.com/labstack/echo/v4"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -21,7 +22,7 @@ func isValidBookingDate(datetimeStr string) bool {
 		return false
 	}
 
-	// must be between 9-5
+	// must be between 9am-5pm
 	extractedTime, _ := time.Parse(TIME_FORMAT, datetime.Format("15:04:05"))
 	openingTime, _ := time.Parse(TIME_FORMAT, "09:00:00")
 	closingTime, _ := time.Parse(TIME_FORMAT, "17:00:00")
@@ -30,25 +31,47 @@ func isValidBookingDate(datetimeStr string) bool {
 	}
 
 	// must be on the hour
+	timeToNearestHour := extractedTime.Truncate(time.Hour)
+	if !extractedTime.Equal(timeToNearestHour) {
+		return false
+	}
+
+	// must be in the future
+	ukLocation, _ := time.LoadLocation("Europe/London")
+	currentDatetimeStr := time.Now().In(ukLocation).Format(DATETIME_FORMAT)
+	if datetimeStr < currentDatetimeStr {
+		return false
+	}
+
 	return true
 }
 
 func CreateBooking(c echo.Context) (err error) {
 	type Body struct {
-		StartDate string `validate:"required,datetime=2006-01-02 15:04:05"`
-		EndDate   string `validate:"required,datetime=2006-01-02 15:04:05"`
+		StartDateTime string `validate:"required,datetime=2006-01-02 15:04:05"`
+		EndDateTime   string `validate:"required,datetime=2006-01-02 15:04:05"`
 	}
 
+	// load body into struct
 	body := new(Body)
 	if err = c.Bind(body); err != nil {
 		return err
 	}
+
+	// validation
 	if err = c.Validate(body); err != nil {
 		return err
 	}
-
-	if !isValidBookingDate(body.StartDate) || !isValidBookingDate(body.EndDate) {
-		return echo.NewHTTPError(http.StatusBadRequest, "startDate and endDate must be on a weekday between 9-5, on the hour and in the future")
+	if !isValidBookingDate(body.StartDateTime) || !isValidBookingDate(body.EndDateTime) {
+		return echo.NewHTTPError(http.StatusBadRequest, "The datetimes must be on a weekday, between 9am-5pm, on the hour and in the future")
+	}
+	if body.EndDateTime <= body.StartDateTime {
+		return echo.NewHTTPError(http.StatusBadRequest, "endDateTime must be greater than startDateTime")
+	}
+	startDate := strings.Split(body.StartDateTime, " ")[0]
+	endDate := strings.Split(body.EndDateTime, " ")[0]
+	if startDate != endDate {
+		return echo.NewHTTPError(http.StatusBadRequest, "The dates must be the same")
 	}
 
 	return c.JSON(http.StatusOK, body)
